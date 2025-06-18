c0de3cf93983d7f63edbb24e03fe002fd9a86d22
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:58:10 +0530

    Create Dowhile1.java

---
+++ Dowhile1.java
@@ -0,0 +1,10 @@
+public class Dowhile1 {
+    public static void main(String[] args) {
+        //do while
+        int k = 0;
+        do {
+            System.out.println(k);
+            k++;
+        }while (k <= 5);
+    }
+}
