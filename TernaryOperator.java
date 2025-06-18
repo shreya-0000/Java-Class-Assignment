1c0041f018f33987c790738c6e0fbaeb5df83b55
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:48:32 +0530

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,10 @@
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        // ternary Operator
+        int d = 30;
+        String res = (d%2 == 0)? "Even number":"odd number";
+        System.out.println(res);
+
+    }
+}
