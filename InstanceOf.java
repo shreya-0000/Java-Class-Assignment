91d2a92d878e058ea63584646d12028ba3bec8bf
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:52:34 +0530

    Create InstanceOf.java

---
+++ InstanceOf.java
@@ -0,0 +1,11 @@
+public class InstanceOf {
+
+    public static void main(String[] args) {
+         // instanceof
+         String str = "Harshini";
+         boolean result = str instanceof String ;
+ 
+         System.out.println("str is instanceof String: " + result);
+ 
+    }
+}
