28b565a5f23a28941d7e3ba7b211bb23e635325d
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:54:49 +0530

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        //for loop
+        for (int j = 0; j <= 10; j = j + 2) {
+            System.out.println(j);
+        }
+    }
+}
