1060c2d558380ccbf8d78227e9467ea4c74712e5
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:53:08 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+    public static void main(String[] args) {
+        //if-else statement
+        int e = 50;
+        int f = 40;
+
+        if(e>f){
+            System.out.println("e is greater than f");
+        }
+        else{
+            System.out.println("f is greater than e");
+        }
+    }
+}
