f31d09a1e870de14b96d9b0f7dde3275a107eca6
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:54:12 +0530

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,12 @@
+public class Function {
+
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int x=5;
+        int y=6;
+        int result= add(x,y);
+        System.out.println(result);
+    }
+}
