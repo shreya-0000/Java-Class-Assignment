3a7e52ab91c4ffa6fde11d1c176e441db5a4d3dd
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:59:12 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,9 @@
+public class Concatenation {
+    public static void main(String[] args){
+        // Concatenation 
+        String a = "hello";
+        String b = "Harshini";
+        String c = a + " " + b;
+        System.out.println("Output is " + c);        
+    }
+}
