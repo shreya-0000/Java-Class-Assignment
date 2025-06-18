90937df3342d5cf3c1d3b6d9d171893d1eba657b
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:47:40 +0530

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        //While loop
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
