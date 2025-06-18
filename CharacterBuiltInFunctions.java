dcd3d7bfa44b345af04e5f34ad34da2a7b7887c7
shreya-0000 <shreyababde@gmail.com>
2025-06-18 13:59:42 +0530

    Create CharacterBuiltInFunctions.java

---
+++ CharacterBuiltInFunctions.java
@@ -0,0 +1,7 @@
+public class CharacterBuiltInFunctions {
+    public static void main(String[] args) {
+        System.out.println(Character.isDigit('5'));
+        System.out.println(Character.isLetter('A'));
+        System.out.println(Character.toUpperCase('a'));
+    }
+}
